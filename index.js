6e3bac1a5040a188c1877b50082e5c0a0a3357b8
Ali <daminovmuhammadali3@gmail.com>
2024-03-25 18:11:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function greetPerson() {
+    console.log(`Hello ${'Sevinch'}`);
+}
+
+
+greetPerson()
+
+
+
+
+
+
+function brack(a, b) {
+
+    let x = a / 100;
+    let y = b / x
+
+    return y;
+}
+
+let a = 1000;
+let b = 15;
+let result = brack(a, b);
+console.log(`${result}%`);
+
